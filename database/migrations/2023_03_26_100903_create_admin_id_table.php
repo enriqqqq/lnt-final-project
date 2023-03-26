394553aa91b1612a51c73778d6d20734626fbe0e
Enrique Heryanto <108193211+enriqqqq@users.noreply.github.com>
2023-03-26 21:39:38 +0700

    Created user & admin database and middleware

--- database/migrations/2023_03_26_100903_create_admin_id_table.php
+++ database/migrations/2023_03_26_100903_create_admin_id_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin_id', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin','user'])->default('user');
-            $table->rememberToken();
+            $table->string('admin_id');
             $table->timestamps();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admin_id');
     }
 };
